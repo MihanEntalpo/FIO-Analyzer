2db1f7e19eac9e67665973c22e9b9f17b8a2b7b8
MihanEntalpo <mihanentalpo@yandex.ru>
2017-01-18 22:36:35 +0700

    Test code

---
+++ test/test.php
@@ -0,0 +1,30 @@
+<?php
+require_once("./vendor/autoload.php");
+$ffs = new \Mihanentalpo\FastFuzzySearch\FastFuzzySearch(array(
+	 'Abbott', 'Abe', 'Addison', 'Adrian', 'Aiken', 'Ainsley', 'Al', 'Alan', 
+    'Alaric', 'Alban', 'Albert', 'Albion', 'Aldrich', 'Alec', 'Alex', 'Alexander', 
+    'Alexis', 'Alf', 'Alfie', 'Alfred', 'Alger', 'Algernon', 'Alick', 'Allan', 
+    'Allen', 'Alton', 'Alvin', 'Ambrose', 'Andrew', 'Andy', 'Anthony', 'Archer', 
+    'Armstrong', 'Arnold', 'Ashley', 'Aston', 'Atwater', 'Aubrey', 'Austin', 
+    'Avery', 'Bailey', 'Baldwin', 'Barclay', 'Barrett', 'Bartholomew', 'Barton', 
+    'Basil', 'Baxter', 'Baz', 'Benedict', 'Benjamin', 'Bennett', 'Benson', 'Bentley', 
+    'Berkley', 'Bernard', 'Bert', 'Bill', 'Blake', 'Bob', 'Bobby', 'Bond', 'Brad', 
+    'Bradley', 'Brent', 'Bret', 'Brewster', 'Brian', 'Brigham', 'Brooke', 'Bruce', 
+    'Bruno', 'Bryant', 'Buck', 'Bud', 'Burgess', 'Burton', 'Byron', 'Cade', 'Caesar', 
+    'Caldwell', 'Calvert', 'Calvin', 'Carl', 'Carlton', 'Carter', 'Carver', 'Cary', 
+    'Casey', 'Cassian', 'Cecil', 'Cedric', 'Chad', 'Chandler', 'Chapman', 'Charles', 
+    'Charlie', 'Charlton', 'Chase', 'Chester', 'Chris', 'Christian', 'Christopher', 
+    'Chuck', 'Clarence', 'Claude', 'Clay', 'Clayton', 'Clement', 'Cliff', 'Clifford', 
+    'Clifton', 'Clive', 'Clyde', 'Cole', 'Coleman', 'Colin', 'Conrad', 'Constant', 
+    'Conway', 'Corwin', 'Courtney', 'Craig', 'Crispin', 'Crosby', 'Curtis', 'Cuthbert', 'Cyril'
+));
+
+
+$input = "charter";
+
+//Lets get three most similiar english names:
+$results = $ffs->find($input, 3);
+
+//End output it:
+print_r($results);
+
---
+++ test/test_single_analyze.php
@@ -0,0 +1,15 @@
+<?php
+require_once(__DIR__ . "/../vendor/autoload.php");
+require_once(__DIR__ . "/../src/FioAnalyzer.php");
+
+$t = microtime(true);
+$fa = new \Mihanentalpo\FioAnalyzer\FioAnalyzer();
+$t = microtime(true) - $t;
+echo "Time for FioAnalyzer initialization: " . $t . "\n";
+
+
+echo "Анализ строки на наличие ФИО:\n";
+print_r($fa->break_apart("Главный инженер Иванов Иван Иванович"));
+
+$ffs = new \Mihanentalpo\FastFuzzySearch\FastFuzzySearch();
+
